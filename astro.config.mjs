89b4980b7d3443e65a6c1e59179c33cabf109365
Vortriz <vorarishi22+github@gmail.com>
2025-12-28 19:25:36 +0530

    improve tooltip
    - fix fonts
    - fix theme toggle
    - refactor
    - biome lints and format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
-import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://vortriz.github.io",
     integrations: [mdx()],
+    experimental: {
+        svgo: true,
+    },
 });
